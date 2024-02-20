68b4893a7f2d4f771e30422d59cd7d290b1edc14
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2024-02-20 11:52:35 +0100

    Add user script

---
+++ script/script.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Bitbucket server need-work-pr highlighter
+// @namespace   rogama25
+// @version     1.0
+// @author      rogama25
+// @description Highlights when a PR needs work in Bitbucket server
+// ==/UserScript==
+
+window.addEventListener('load', async function() {
+    const result = await fetch("/rest/api/latest/dashboard/pull-requests?limit=1000&state=OPEN")
+    const data = await result.json()
+
+    const paint = function () {
+        const prRows = document.querySelectorAll(".dashboard-pull-request-table .pull-request-row")
+        data.values.forEach(pr => {
+            if (pr.reviewers.some(r => r.status === "NEEDS_WORK")) {
+                const row = Array.from(prRows).find(row => row.querySelector("a").href.includes(pr.links.self[0].href))
+                if (!row) return
+                row.style.backgroundColor = "rgba(255, 0, 0, 0.2)"
+            }
+        })
+    }
+    paint()
+    this.document.querySelectorAll(".show-more").forEach(btn => btn.addEventListener("click", () => setTimeout(paint, 100)))
+});
